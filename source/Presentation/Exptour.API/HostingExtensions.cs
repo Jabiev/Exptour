c95164eb3f25a45fbeadc6d2d98c7b74df755395
DESKTOP-1424DPE\HP <jabieviam@gmail.com>
2025-02-03 12:40:32 +0400

    upgraded architecture

---
+++ source/Presentation/Exptour.API/HostingExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Exptour.API;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        return builder.Build();
+    }
+}
--- source/Presentation/Exptour.API/Program.cs
+++ source/Presentation/Exptour.API/Program.cs
@@ -6,13 +6,39 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerDocument(configure =>
+{
+    configure.PostProcess = (doc =>
+    {
+        doc.Info.Title = "Explore Tour Guide";
+        doc.Info.Version = "1.0.0";
+        doc.Info.Description = "Booking all you need | Cars, Guides, Hotels, Ready Packages and everything you need";
+        doc.Info.Contact = new NSwag.OpenApiContact()
+        {
+            Name = "E x p t o u r",
+            Url = "https://www.youtube.com/@iamjabiev",
+            Email = "jabieviam@gmail.com"
+        };
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapGet("/", handler =>
+{
+    handler.Response.Redirect("/swagger/index.html", permanent: false);
+    return Task.CompletedTask;
+});
 
 app.UseHttpsRedirection();
 
